54fcd13a15a853f79ae932aa5da8996b3f0eb70e
John Rodriguez <johnrodricres@gmail.com>
2022-07-03 00:06:55 -0400

    Completed exercises 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheitToCelsius) {
+  fahrenheitToCelsius = (fahrenheitToCelsius - 32) /1.8;
+  return Number(fahrenheitToCelsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusToFahrenheit) {
+  celsiusToFahrenheit = (celsiusToFahrenheit * 1.8) + 32;
+  return Number(celsiusToFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
