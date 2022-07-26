dc27cc6536189fa60ed29db7306e6438bcc44a34
John Rodriguez <johnrodricres@gmail.com>
2022-07-26 16:47:20 -0400

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(str) {
+    const a = Number(str)
+    if (a < 1) return 'OOPS'
+    let fibonacci = 1;
+    let lastNumber = 0;
+    let previousFib = 0;
 
+    for(let i = 2; i <= a; i++){
+        lastNumber = fibonacci;
+        fibonacci = fibonacci + previousFib;
+        previousFib = lastNumber;
+    }
+    return fibonacci
 };
 
 // Do not edit below this line
