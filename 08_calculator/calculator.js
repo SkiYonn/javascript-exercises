8c3ec16987ed4fabde666995baaea1accb6d76cb
John Rodriguez <johnrodricres@gmail.com>
2022-07-26 16:47:09 -0400

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  // 0 is the inicial value
+  return array.reduce(
+    (previousValue, currentValue) => previousValue + currentValue, 0)
 };
 
-const multiply = function() {
+const multiply = function(array) {
 
+  return array.length
+    ? array.reduce(
+    (previousValue, currentValue) => previousValue * currentValue ) : 0
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i++){
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
